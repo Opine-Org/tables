a8aab9513611a2c9c8638f95d0dad8c2e7026078
Ryan Mahoney <ryan@virtuecenter.com>
2016-12-09 20:28:40 -0500

    Update admin.sql

--- admin.sql
+++ admin.sql
@@ -1,1 +1,20 @@
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.modified = now();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
 
+CREATE TABLE admin_widgets (
+    id SERIAL NOT NULL primary key,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    icon VARCHAR(100),
+    link VARCHAR(250),
+    roles_ids INTEGER[],
+    created TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+    modified TIMESTAMP
+);
+
+CREATE TRIGGER update_widget_modtime BEFORE UPDATE ON widgets FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
