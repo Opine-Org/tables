b8818bdb45ff8acd89ea2550ce7556cfa2f0ec79
Ryan Mahoney <ryan@virtuecenter.com>
2016-12-17 18:50:03 -0500

    Update ecommerce.sql

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,1 +1,18 @@
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.modified = now();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
 
+CREATE TABLE ecommerce_cart_items (
+    id SERIAL NOT NULL primary key,
+    physical_product_id integer,
+    session_id char(36),
+    order_id integer,
+    created TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+    modified TIMESTAMP
+);
+
+CREATE TRIGGER update_ecommerce_cart_items_modtime BEFORE UPDATE ON ecommerce_cart_items FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
