0b086f2e2c6e20cc03c6888917b00a391a002d1e
Ryan Mahoney <ryan@virtuecenter.com>
2016-12-17 22:51:25 -0500

    Update ecommerce.sql

--- ecommerce.sql
+++ ecommerce.sql
@@ -6,19 +6,70 @@ BEGIN
 END;
 $$ language 'plpgsql';
 
-CREATE TABLE ecommerce_orders ();
+CREATE TABLE ecommerce_orders (
+    id SERIAL NOT NULL primary key,
+    user_id INTEGER,
+    session_id CHAR(32),
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified TIMESTAMP,
+    status VARCHAR(100),
+    deleted TIMESTAMP
+);
 
-CREATE TABLE ecommerce_transactions ();
+CREATE TABLE ecommerce_product_categories (
+    id SERIAL NOT NULL primary key,
+    name VARCHAR(200) NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified TIMESTAMP,
+    deleted TIMESTAMP
+);
+
+CREATE TABLE ecommerce_products (
+    id SERIAL NOT NULL primary key,
+    name VARCHAR(200) NOT NULL,
+    slug VARCHAR(200) NOT NULL,
+    description TEXT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified TIMESTAMP,
+    price_high DECIMAL,
+    price_low DECIMAL,
+    status VARCHAR(32),
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified TIMESTAMP,
+    deleted TIMESTAMP
+);
+
+CREATE TABLE ecommerce_product_categories_map (
+    id SERIAL NOT NULL primary key,
+    category_id INTEGER NOT NULL REFERENCES ecommerce_product_categories,
+    product_id INTEGER NOT NULL REFERENCES ecommerce_products,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted TIMESTAMP
+);
+
+CREATE TABLE ecommerce_products_physical (
+    id SERIAL NOT NULL primary key,
+    product_id INTEGER NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    slug VARCHAR(200) NOT NULL,
+    sku VARCHAR(100) NOT NULL,
+    price DECIMAL,
+    price_original DECIMAL,
+    price_wholesale DECIMAL,
+    status VARCHAR(32),
+    sort_key SMALLINT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified TIMESTAMP
+);
+
+CREATE TABLE ecommerce_transactions (
+);
 
 CREATE TABLE ecommerce_sales_reps ();
 
-CREATE TABLE ecommerce_customers ();
+
 
 CREATE TABLE ecommerce_product_categories ();
-
-CREATE TABLE ecommerce_products_logical ();
-
-CREATE TABLE ecommerce_products_physical ();
 
 CREATE TABLE ecommerce_order_items (
     id SERIAL NOT NULL primary key,
